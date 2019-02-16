d013ec0baf35890c74801b9f0057bc7c3dd4ad49
James Cho <hwisoocho@gmail.com>
2019-02-15 19:48:27 -0800

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+const footerStyle = {
+  backgroundColor: '#003399',
+  fontSize: '15px',
+  color: 'white',
+  borderTop: '1px solid #E7E7E7',
+  textAlign: 'center',
+  padding: '20px',
+  position: 'fixed',
+  left: '0',
+  bottom: '0',
+  height: '30px',
+  width: '100%'
+};
+
 function Footer() {
   return (
-    <div id="footerContainer">
-      <h4 id="headerMain">JC</h4>
+    <div style={footerStyle}>
+      <h4>James Cho</h4>
     </div>
   );
 }
