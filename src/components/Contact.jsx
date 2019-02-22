49072a6b8b91be3f567d487911389786d6257e0c
James Cho <hwisoocho@gmail.com>
2019-02-22 10:49:35 -0800

    Create contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div id="contacttMain">
+      <style jsx>
+        {`
+          #contactMain {
+            width: 60%;
+            margin: auto;
+          }
+        `}
+      </style>
+      <h1>Contact</h1>
+      <h2>Hi! </h2>
+
+      <p>lorem</p>
+    </div>
+  );
+}
+
+export default Contact;
