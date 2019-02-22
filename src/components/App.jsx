c56365866b8f403db629f32b57bf7aba34eaa1bb
James Cho <hwisoocho@gmail.com>
2019-02-22 10:40:30 -0800

    make App stateful component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,23 @@ import Error404 from './Error404';
 import Footer from './Footer';
 // import globalStyles from '../assets/styles/global.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={ProjectsList} />
-        <Route component={Error404} />
-      </Switch>
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={ProjectsList} />
+          <Route component={Error404} />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
